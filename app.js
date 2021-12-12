f8eb4da568a28553069d0b925dbcce3700db64c7
Nyanjuimarvin <marnyanjui@gmail.com>
2021-12-12 11:24:19 +0300

    Define day calculation logic

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ const yearInput = form.elements.year;
 const dateVal = dateInput.value;
 const monthVal = monthInput.value;
 const yearVal = yearInput.value;
+
+//Function for calculating Day
+const calculateDay = ( dateVal, monthVal, yearVal )=>{
+    var year_String = yearVal.toString();
+    var century_Digit = year_String.substr(0,2);
+    var year_Digit = century_Digit.substr(2,4);
+    return  (((parseInt( century_Digit)/4 ) - 2 * ( parseInt( century_Digit ) - 1) +
+            (5 * (parseInt( year_Digit ) / 4)) + (26 * ( parseInt( monthVal )+1) / 10) +
+            parseInt( dateVal )) % 7);
+            
+}
