6233f0f24b509f5e13064a64caff9266c076d1fb
Nyanjuimarvin <marnyanjui@gmail.com>
2021-12-12 10:43:32 +0300

    Define input value Variables

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-//define form object
+//define form object and variables
 const form = document.querySelector('form');
 const dateInput = form.elements.date;
 const monthInput = form.elements.month;
 const yearInput = form.elements.year;
 
+//Define value variables
+const dateVal = dateInput.value;
+const monthVal = monthInput.value;
+const yearVal = yearInput.value;
